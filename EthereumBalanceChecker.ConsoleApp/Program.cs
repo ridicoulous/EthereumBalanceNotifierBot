f6335c6da84f8c7e4d1ab3c04ddce67bc5814723
msg_kurt <artemkuryanov@gmail.com>
2019-09-21 21:49:56 +0300

    added manual reset event

--- EthereumBalanceChecker.ConsoleApp/Program.cs
+++ EthereumBalanceChecker.ConsoleApp/Program.cs
@@ -4,11 +4,13 @@ using System;
 using Microsoft.Extensions.Configuration.FileExtensions;
 using Microsoft.Extensions.Configuration.Json;
 using System.IO;
+using System.Threading;
 
 namespace EthereumBalanceChecker.ConsoleApp
 {
     class Program
     {
+        static ManualResetEvent _quitEvent = new ManualResetEvent(false);
         static void Main(string[] args)
         {
 
@@ -19,9 +21,8 @@ namespace EthereumBalanceChecker.ConsoleApp
             var key = config["BotKey"];
             var bot = new EtherBalanceBot(key, int.Parse(admin), new System.Collections.Generic.List<string>() { "Address to check","Balance lower than"});
             bot.Run();
-            //AddressChecker d = new AddressChecker();
-            //d.Lookup();
-            Console.Read();
+            _quitEvent.WaitOne();       
+          
         }
     }
 }
